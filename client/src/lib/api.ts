e0b5a0c85b030bdd9bab002a6446d991303e0851
khaled-badenjki <m.k.badenjki2@gmail.com>
2025-08-15 14:31:36 +0300

    fix

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -9,18 +9,13 @@ export const api = {
       if (params?.city) searchParams.append('q[city_eq]', params.city);
       if (params?.size) searchParams.append('q[size_eq]', params.size);
       
-      const url = `http://localhost:3000/mosques.json${searchParams.toString() ? `?${searchParams}` : ''}`;
+      const url = `https://masajid-admin.rabeh.sy/mosques.json${searchParams.toString() ? `?${searchParams}` : ''}`;
       const response = await apiRequest('GET', url);
       return response.json();
     },
 
     getById: async (id: string): Promise<MosqueWithDonations> => {
-      const response = await apiRequest('GET', `http://localhost:3000/mosques/${id}.json`);
-      return response.json();
-    },
-
-    getDonations: async (id: string): Promise<Donation[]> => {
-      const response = await apiRequest('GET', `https://8c5e51da-5a16-4d16-80d2-f9dc5f1086e2-00-25czk4ja6iuyf.spock.replit.dev/api/mosques/${id}/donations`);
+      const response = await apiRequest('GET', `https://masajid-admin.rabeh.sy/mosques/${id}.json`);
       return response.json();
     }
   }
